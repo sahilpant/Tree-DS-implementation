7ff229893d6a1658b2e5275c044737586ebd9c2e
Sahil Pant <sahilpant16@hotmail.com>
2020-07-07 12:39:21 +0530

    added basic layout of the program

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    TreeNode *left;
+    int data;
+    TreeNode *right;
+}*root;
+
+typedef TreeNode node;
+
+int main()
+{
+    
+}
